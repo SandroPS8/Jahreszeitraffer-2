2538e479fed8a20c8742c32378d0f1ad52349d43
SandroPS8 <53835517+SandroPS8@users.noreply.github.com>
2019-09-19 21:11:55 +0200

    Add files via upload

--- LCD-Anzeige.ino
+++ LCD-Anzeige.ino
@@ -1,6 +1,7 @@
 #include <LiquidCrystal.h>
 #include <Wire.h>
 #include "DS3231.h"
+#include <EEPROM.h>
 RTClib RTC;
 
 const int RelayRead = 8;
@@ -10,10 +11,9 @@ const int CamPWRRead = 6;
 
 unsigned  long lastMillis1;
 
-
-int counter = 171;
+int Frames;
 int counter2 = 0;
-int CamPWRHIGH = 0;
+int CamPWRHIGH;
 int nowday;
 int RelayReadHIGH = 0;
   
@@ -23,13 +23,18 @@ LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 void setup (){
   Wire.begin();
   lcd.begin(16, 2);
-  
+  DateTime now = RTC.now();
+    
   pinMode(RelayRead, INPUT); //Messpin für Auslösereleis
   pinMode(LCDButton, INPUT); //Messpin für Display an
   pinMode(CamPWRRead, INPUT); //Messpin für Kamerastrom
   pinMode(LCD, OUTPUT); //Wenn HIGH,  LCD Hintergrundbeleuchtung an
   
   lastMillis1 = millis ();
+
+  Frames = EEPROM.read(0);
+  CamPWRHIGH = EEPROM.read(1);
+  nowday = EEPROM.read(2);
   
 }
  
@@ -43,42 +48,63 @@ if(digitalRead(RelayRead))
  } 
 if(!digitalRead(RelayRead) && RelayReadHIGH == 1)
  {
-   counter++;
+   Frames++;
    counter2++;
    RelayReadHIGH = 0;
  }
+
  
   //Lässt das Display bei Knopfdruck 15 Sekunden aufleuchten
  {
-if(digitalRead(LCDButton)) {
+if(digitalRead(LCDButton)) 
+  {
     lastMillis1 = millis ();
   }
-if(millis () - lastMillis1 <= 15000) {
+if(millis () - lastMillis1 <= 15000) 
+  {
     digitalWrite(LCD, HIGH);
   }
 else
   {
     digitalWrite(LCD, LOW);
   }
+
   
   //Wenn Auslösungen geschehen, leuchtet ein X auf
 {
-  if(digitalRead(CamPWRRead) && counter2 >= 8 && RelayReadHIGH == 1)
+if(digitalRead(CamPWRRead) && !digitalRead(RelayRead) && counter2 >= 9)
   {
     CamPWRHIGH = 1;
     nowday = now.day();
   }
-lcd.setCursor(15,1);
-  if(CamPWRHIGH == 1 && nowday == now.day()){
-    lcd.print("X");
-    counter2 = 0;
-  }
-}
 
+lcd.setCursor(15,1);
+  if(CamPWRHIGH == 1 && nowday == now.day()) 
+  {
+    lcd.print("X");
+  }
+
+else
+  {
+    lcd.setCursor(15,1);
+    lcd.print(" ");
+  }
+       
+// Speichert alle relevanten Werte 3 Sekunden nach abschalten der Kamera im EEPROM.  
+if(!digitalRead(CamPWRRead) && counter2 >= 8)
 { 
+  delay(250);
+  EEPROM.write(0, Frames); 
+  EEPROM.write(1, CamPWRHIGH);
+  EEPROM.write(2, nowday);
+  delay(250);
+  counter2 = 0;
+}
+  }
   
+{  
 // Logik des Ganzen (Zeigt aktulle Uhrzeit der RTC, belichtete Frame Anzahl und ob an dem Tag bereits ausgelöst wurde.)
-{ 
+  { 
   //Obere Zeile: Zeit      
 lcd.setCursor(0,0);
 lcd.print("Time:  ");
@@ -93,8 +119,12 @@ lcd.print(now.second());
 
 //Untere Zeile: Frames und ob bereits ausgelöst wurde
 lcd.setCursor(0,1); 
-lcd.print("Frames:   ");   
-lcd.print(counter);
+lcd.print("Frames:   ");
+//lcd.print("Frames:  ");
+//if(Frames <10) lcd.print("000");
+//if(Frames <100) lcd.print("00");
+//if(Frames <1000) lcd.print("0");   
+lcd.print(Frames);
 
 delay(210);
 }
